867b67615836150e42efc7f2b98079afa2d85b37
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-10-10 21:53:21 +0300

    quick reformatting

--- Caeser Cipher/caeser.py
+++ Caeser Cipher/caeser.py
@@ -1,6 +1,7 @@
 '''
 Description:
-You are to write a function that accepts two arguments, a plain-text message and a number of letters to shift in the cipher. The function will return an encrypted string with all letters transformed and all punctuation and whitespace remaining unchanged.
+You are to write a function that accepts two arguments, a plain-text message and a number of letters to shift in the cipher.
+The function will return an encrypted string with all letters transformed and all punctuation and whitespace remaining unchanged.
 
 Note: You can assume the plain text is all lowercase ASCII except for whitespace and punctuation.
 '''
