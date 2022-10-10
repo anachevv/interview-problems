9d2598a581cbc76891980e22f55e91f4928b22af
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-10-10 21:51:42 +0300

    description added

--- Caeser Cipher/caeser.py
+++ Caeser Cipher/caeser.py
@@ -1,3 +1,11 @@
+'''
+Description:
+You are to write a function that accepts two arguments, a plain-text message and a number of letters to shift in the cipher. The function will return an encrypted string with all letters transformed and all punctuation and whitespace remaining unchanged.
+
+Note: You can assume the plain text is all lowercase ASCII except for whitespace and punctuation.
+'''
+
+
 def encrypt(msg, shift):
     alpha = [chr(x) for x in range(97, 122 + 1)]
     msg_lst = [x for x in msg]
